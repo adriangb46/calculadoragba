3c7b1733c062009a417781c5cdfa77e923a19df3
Adrián González Blanco <z3u2thnder@proton.me>
2026-04-23 19:26:41 +0200

    first commit

---
+++ src/main/java/com/example/calculadoragba/CalculadoragbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadoragba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoragbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoragbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculadoragba/CalculadoragbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadoragba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoragbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

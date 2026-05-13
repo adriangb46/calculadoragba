6dcad0a24e28a17de778cb337696e3ac50343ee0
Adrián González Blanco <z3u2thnder@proton.me>
2026-05-13 10:33:25 +0200

    chore: integrate checkstyle plugin into build process and enforce coding standards in CI workflow

--- src/main/java/com/example/calculadoragba/CalculadoragbaApplication.java
+++ src/main/java/com/example/calculadoragba/CalculadoragbaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadoragbaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalculadoragbaApplication.class, args);
-	}
-
+public static void main(String[] args) {
+SpringApplication.run(CalculadoragbaApplication.class, args);
+}
 }
